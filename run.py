a450c56fc5c612077fd867c89bd01204bc97a65c
christian-regnante <c.ngwino@alustudent.com>
2026-03-29 22:10:56 +0200

    changed the ports and debug for deployment

--- run.py
+++ run.py
@@ -17,4 +17,5 @@ app = create_app()
 if __name__ == '__main__':
     # Run the development server. Debug=True is suitable for local
     # development only.
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run()
